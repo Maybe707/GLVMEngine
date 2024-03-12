6d2fac248343e392c48b20ba605f32ec4e7e3cd0
Yuriorkis_Scream <fellfrostqtw@gmail.com>
2024-03-12 14:00:05 +0900

    Add header licenses to all files

--- include/UnixApi/WindowXCBVulkan.hpp
+++ include/UnixApi/WindowXCBVulkan.hpp
@@ -1,3 +1,8 @@
+// This file is part of Game Loop Versatile Modules (GLVM)
+// Copyright © 2024 Maksim Manokhin a.k.a. Yuriorkis_Scream. Contacts: <fellfrostqtw@gmail.com>
+// Author: Maksim Manokhin a.k.a. Yuriorkis_Scream
+// License: http://opensource.org/licenses/MIT
+
 #ifndef WINDOW_XCB_VULKAN
 #define WINDOW_XCB_VULKAN
 
