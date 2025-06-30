dcf6726118cd62667bdabb8bd565275216931539
Scream9 <fellfrostqwt@gmail.com>
2025-06-30 16:50:29 +0900

    Fix new vulkan warning after update

--- src/GraphicAPI/Vulkan.cpp
+++ src/GraphicAPI/Vulkan.cpp
@@ -4920,7 +4920,7 @@ namespace GLVM::core
 											std::vector<VkSemaphore>& renderFinishedSemaphores,
 											std::vector<VkFence>& inFlightFences) {
         imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
-        renderFinishedSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+        renderFinishedSemaphores.resize(swapChainImages.size());
         inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
 		
         VkSemaphoreCreateInfo semaphoreInfo{};
@@ -4934,6 +4934,12 @@ namespace GLVM::core
             if (vkCreateSemaphore(device, &semaphoreInfo, nullptr, &imageAvailableSemaphores[i]) != VK_SUCCESS ||
                 vkCreateSemaphore(device, &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) != VK_SUCCESS ||
                 vkCreateFence(device, &fenceInfo, nullptr, &inFlightFences[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to create synchronization objects for a frame!");
+            }
+        }
+
+		for (size_t i = 0; i < swapChainImages.size(); i++) {
+            if ( vkCreateSemaphore(device, &semaphoreInfo, nullptr, &renderFinishedSemaphores[i]) != VK_SUCCESS ) {
                 throw std::runtime_error("failed to create synchronization objects for a frame!");
             }
         }
@@ -5451,7 +5457,7 @@ namespace GLVM::core
         submitInfo.commandBufferCount = 1;
         submitInfo.pCommandBuffers = &mainRenderCommandBuffers[currentFrame];
 
-        VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[currentFrame]};
+        VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[imageIndex]};
         submitInfo.signalSemaphoreCount = 1;
         submitInfo.pSignalSemaphores = signalSemaphores;
 
